3663dda7438b8c2d3fb047b3055b07da6c956541
geoff brooks <geoff.brooks@ul.com>
2020-06-05 06:59:07 -0400

    Add project files.

---
+++ Shared/Entities/XProject.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RuleEditor.Shared.Entities
+{
+    public class XProject
+    {
+        public XProject()
+        {
+
+        }
+
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+
+
+
+    }
+}
---
+++ Shared/Entities/XRule.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RuleEditor.Shared.Entities
+{
+    public class XRule
+    {
+
+        public XRule()
+        {
+
+        }
+
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int Priority { get; set; }
+
+        public string Description { get; set; }
+
+        public List<string> Tags { get; set; }
+
+        public int Repeatability { get; set; }
+
+        public string Body { get; set; }
+
+        public bool IsActive { get; set; }
+
+    }
+}
